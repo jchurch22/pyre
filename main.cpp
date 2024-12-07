ffb93a09f2d0604edb91176416e1019afef536a5
Johnathan Church <JCHURCH1@cub.uca.edu>
2024-12-07 16:39:18 -0600

    Commit main.cpp; accidentally missing in previous commit

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ void menu(LinkedList<Character>& characterList, std::string file);
 int inputValidation();
 void clearScreen();
 void loadCharacters(const std::string& file, LinkedList<Character>& characterList);
+void printCharacters(const LinkedList<Character>& list);
 void addCharacter(LinkedList<Character>& characterList);
 void updateFile(const std::string& file, LinkedList<Character>& characterList, bool overwrite);
 
@@ -27,6 +28,9 @@ int main() {
 
 	std::string file = "characters.txt";
 
+	// Load character menu.
+	loadCharacters(file, characterList);
+
 	// Display menu.
 	menu(characterList, file);
 
@@ -34,16 +38,125 @@ int main() {
 
 void menu(LinkedList<Character>& characterList, std::string file) {
 
-	int choice;
+	while (true) {
 
-	// Display menu.
-	std::cout << "Pyre v.1.0" << std::endl << std::endl;
+		// Variable declaration
+		std::string name;
+		
+		// Clear screen.
+		clearScreen();
 
-	std::cout << "1. Display Characters" << std::endl
-		      << "2. Add Character" << std::endl
-		      << "3. Remove Character" << std::endl
-		      << "4. About" << std::endl
-	 	      << "5. Exit" << std::endl << std::endl;
+		// Display menu.
+		std::cout << "Pyre v.1.0" << std::endl << std::endl;
+
+		std::cout << "1. Display Characters" << std::endl
+				  << "2. Add Character" << std::endl
+				  << "3. Remove Character" << std::endl
+				  << "4. About" << std::endl
+	 			  << "5. Exit" << std::endl << std::endl;
+
+		// Accept user input.
+		int choice = inputValidation();
+		clearScreen();
+
+		switch (choice) {
+
+		case 1:
+
+			// Display characters.
+			std::cout << "Characters" << std::endl << std::endl;
+
+			if (characterList.empty())
+				std::cout << "No characters found." << std::endl << std::endl;
+			else
+				printCharacters(characterList);
+
+			// Return to the main menu.
+			std::cout << "Press ENTER to return to the menu." << std::endl;
+			std::cin.ignore();
+			std::cin.get();
+			break;
+
+		case 2:
+
+			// Refresh input buffer.
+			std::cin.ignore();
+
+			// Add a character.
+			std::cout << "Add Character" << std::endl << std::endl;
+			addCharacter(characterList);
+			updateFile(file, characterList, false);
+
+			// Return to the main menu.
+			std::cout << "Press ENTER to return to the menu." << std::endl;
+			std::cin.ignore();
+			break;
+
+		case 3:
+
+			// Refresh input buffer.
+			std::cin.ignore();
+
+			// Remove a character
+			std::cout << "Remove Character" << std::endl << std::endl
+			          << "Please enter the name of the character you'd like to delete: ";
+			std::getline(std::cin, name);
+
+			if (characterList.deleteCharacter(name))
+				std::cout << std::endl << "Character " << name << " successfully deleted." << std::endl << std::endl;
+			else
+				std::cout << std::endl << std::endl << "Character " << name << " not found." << std::endl << std::endl;
+
+			// Update file.
+			updateFile(file, characterList, true);
+
+			// Return to the main menu.
+			std::cout << "Press ENTER to return to the menu." << std::endl;
+			std::cin.ignore();
+			break;
+
+		case 4:
+
+			// Refresh input buffer.
+			std::cin.ignore();
+
+			// About page.
+			std::cout << "Welcome to Pyre v.1.0." << std::endl << std::endl
+				      << "Using this program, you will be able to build a catalog of the characters you've created," << std::endl
+				      << "be it for a fantasy world of your own or a collaborative roleplaying experience with others." << std::endl << std::endl
+				      << "To create a character, simply enter their name, species, age, and a short description. Your characters" << std::endl
+				      << "will be safely stored in a file saved in this program's directory for you to view and remove at your leisure." << std::endl << std::endl
+				      << "Happy creating!" << std::endl << std::endl
+				      << "Created by: Johnathan Church" << std::endl
+				      << "CSCI 2320: Data Structures" << std::endl
+				      << "Project Advisor: Patricia Talley" << std::endl << std::endl;
+
+			// Return to the main menu.
+			std::cout << "Press ENTER to return to the menu." << std::endl;
+			std::cin.ignore();
+			break;
+
+		case 5:
+
+			// Exit
+			std::cout << "Thank you for using Pyre." << std::endl;
+			exit(0);
+
+		default:
+
+			// Refresh input buffer.
+			std::cin.ignore();
+
+			std::cout << "Please enter an integer 1-5 to make a selection." << std::endl << std::endl;
+
+			// Return to the main menu.
+			std::cout << "Press ENTER to return to the menu." << std::endl;
+			std::cin.ignore();
+			break;
+
+		}
+
+	}
 
 }
 
@@ -129,6 +242,22 @@ void loadCharacters(const std::string& file, LinkedList<Character>& characterLis
 
 }
 
+// This function will print the attributes of each character in the list.
+void printCharacters(const LinkedList<Character>& list) {
+
+	list.traverse([](const Character& character) {
+
+		std::cout << "--------------------------------" << std::endl << std::endl
+			      << "Name: " << character.name << std::endl
+		          << "Species: " << character.species << std::endl
+		          << "Age: " << character.age << std::endl << std::endl
+		          << "Description:" << std::endl << std::endl << character.description << std::endl << std::endl
+			      << "--------------------------------" << std::endl << std::endl;
+
+	});
+
+}
+
 // This function will add a character to the file.
 void addCharacter(LinkedList<Character>& characterList) {
 
@@ -145,12 +274,13 @@ void addCharacter(LinkedList<Character>& characterList) {
 	newCharacter.age = inputValidation();
 
 	std::cout << std::endl << "Enter character description: " << std::endl << std::endl;
+	std::cin.ignore();
 	std::getline(std::cin, newCharacter.description);
 
 	// Add character to list.
 	characterList.push_back(newCharacter);
 
-	std::cout << std::endl << std::endl << newCharacter.name << " added." << std::endl;
+	std::cout << std::endl << "Character " << newCharacter.name << " successfully added!" << std::endl << std::endl;
 
 }
 
